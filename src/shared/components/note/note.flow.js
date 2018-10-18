8b551ffa8b1c778c28fc7f20914f1b6aa0b7f27d
davemackintosh <me@davemackintosh.co.uk>
2018-10-18 16:44:52 +0100

    Close enough for one day

--- src/shared/components/note/note.flow.js
+++ src/shared/components/note/note.flow.js
@@ -4,4 +4,7 @@ import type { NoteType } from "@flow/note.flow"
 
 export type Props = {
   note: NoteType,
+  className?: string,
+  onNoteClick: (note: NoteType) => void,
+  onNoteKeyUp: (note: NoteType, keyCode: number) => void,
 }
--- src/shared/components/note/note.js
+++ src/shared/components/note/note.js
@@ -8,14 +8,26 @@ import type { Props } from "./note.flow"
 function Note(props: Props) {
   const classNames = classes({
     note: true,
+    [props.className || ""]: true,
     [`note--string__${props.note.string}`]: true,
   })
 
   return (
-    <span className={ classNames }>
+    <span
+      onClick={ props.onNoteClick.bind(null, props.note) }
+      className={ classNames }
+    >
       { props.note.fret }
     </span>
   )
+}
+
+Note.defaultProps = {
+  onNoteClick: (note: NoteType) =>
+    console.warn("onNoteClick is a noop. Nothing will happen.", note), // eslint-disable-line
+
+  onNoteKeyUp: (note: NoteType, keyCode: number) =>
+    console.warn("onNoteKeyUp is a noop. Nothing will happen.", note, keyCode), // eslint-disable-line
 }
 
 export default Note
